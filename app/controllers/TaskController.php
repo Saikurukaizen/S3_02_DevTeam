4da87a874ee789cba66872a70c00e0bf42be1af4
Marc <mks4nch3z@gmail.com>
2025-07-08 13:18:52 +0200

    Prepare for awaiting layouts

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -7,22 +7,32 @@ class TaskController extends ApplicationController{
     //para asociarse a las rutas (por convención)
     public function indexAction(): void
     {
-
+        /*Acción asociada a la ruta principal del recurso (/task o /task/index)
+        Muestra un listado de todas las tareas.
+        Aquí instancias el modelo, obtienes todas las tareas y las pasas a la vista.
+        Muestra el listado por GET*/
+        $taskModel = new TaskModel();
+        $tasks = $taskModel->getAll();
+        $this->view->tasks = $tasks;
     }
 
-   
+    /*Método para instanciar el TaskModel con los $data al createTask()
     public function createAction(): void
     {
-        
+        /*Acción asociada a la creación de un nuevo elemento
+        Procesa el guardado de la nueva tarea si es POST
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             $data = [
                 'titulo' => $this->_getParam('titulo'),
-                'descripcion' => $this->_getParam('descripcion')
+                'descripcion' => $this->_getParam('descripcion'),
+                'estado' => $this->_getParam('estado')
             ];
             $taskModel = new TaskModel();
-            $taskModel->crearTarea($data);
+            $taskModel->createTask($data);
         }
-    }
+    }*/
+
+    
 
     
 }

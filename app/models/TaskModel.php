a0cc752e89990c882ea7d62df2e80c956c114fba
Marc <mks4nch3z@gmail.com>
2025-07-09 10:16:57 +0200

    creando un json de pruebas

--- app/models/TaskModel.php
+++ app/models/TaskModel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 //Para persistencia en SQL, esta clase extiende del modelo principal
 class TaskModel{
 
-    private $file = 'config/tasks.json';
+    private $file = 'config/fakeTasks.json';
 
     /* public function crearTarea(array $data): void
     {
